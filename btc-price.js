6329558f539ad03cdf2b1709ce2d269bfc9a6ee6
mi <33814019+npocmak@users.noreply.github.com>
2019-11-07 00:17:49 +0200

    Update btc-price.js

--- btc-price.js
+++ btc-price.js
@@ -1,9 +1,9 @@
 const btcPrice = () =>
-  fetch("https://api.coindesk.com/v1/bpi/currentprice/GBP.json")
+  fetch("https://api.coindesk.com/v1/bpi/currentprice/BGN.json")
     .then(res => res.json())
     .then(myJson => {
-      let GBPAmount = myJson["bpi"]["GBP"]["rate"];
-      document.getElementById("btc-price").append(`£${GBPAmount}`);
+      let GBPAmount = myJson["bpi"]["BGN"]["rate"];
+      document.getElementById("btc-price").append(`{BGNAmount}лева`);
     })
     .catch(err => console.log(err));
 
